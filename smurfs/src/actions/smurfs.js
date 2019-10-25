f52ca6f65aa9ef959db3aac10860167d80c86b9b
Ruth Poliakon <r.poliakon@gmail.com>
2019-10-25 17:12:11 -0600

    project initialization - install dependencies, file structure, middleware

---
+++ smurfs/src/actions/smurfs.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_ERROR = "FETCH_SMURF_ERROR";
+
+export function fetchSmurf() {
+  return dispatch => {
+    dispatch({ type: FETCH_SMURF_START });
+
+    axios
+      .get(`/smurfs`)
+      .then(res => {
+        dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+      })
+      .catch(err => {
+        dispatch({ type: FETCH_SMURF_ERROR, payload: err.response });
+      });
+  };
+}
---
+++ smurfs/src/reducers/smurf.js
@@ -0,0 +1,36 @@
+import {
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_ERROR
+} from "../actions/smurfs";
+
+const initialState = {
+  smurfs: {},
+  isLoading: false,
+  error: null
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      };
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false
+      };
+    case FETCH_SMURF_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+}
